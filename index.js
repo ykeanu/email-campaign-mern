aeeb3e9eb3657210da200880d53019fe2df13183
ykeanu <ykhernan@ucla.edu>
2018-01-13 13:45:25 -0800

    Setup remote instance of mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js')
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 // Sets up configuration that will listen for incoming requests 
 const app = express();
@@ -10,4 +14,3 @@ require('./routes/authRoutes')(app);
 // Dynamically figure out what Heroku tells us to listen to 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
