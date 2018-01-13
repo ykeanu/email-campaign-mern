4f5188f488875799f1beaf539c7bcd4f0738b86b
ykeanu <ykhernan@ucla.edu>
2018-01-13 15:45:47 -0800

    Configure cookies for user auth info

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const keys = require('./config/keys.js')
-require('./services/passport');
-require('./models/User')
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys.js");
+// Order matters to load in user model before executing passport
+require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
 
-// Sets up configuration that will listen for incoming requests 
+// Sets up configuration that will listen for incoming requests
 const app = express();
 
 // Require returns a function, and we call it with the (app) object
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
-// Dynamically figure out what Heroku tells us to listen to 
+// Dynamically figure out what Heroku tells us to listen to
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,19 +1,45 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+// Model Class
+const User = mongoose.model("users");
+
+// Stuffs user info into cookie
+passport.serializeUser((user, done) => {
+  // done is a callback we have to call after we've done some work in passport
+  done(null, user.id);
+});
+
+// Pull information out of cookie
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    }, 
+      callbackURL: "/auth/google/callback"
+    },
     (accessToken, refreshToken, profile, done) => {
-      // accessToken is the uniqueID we can save in our database
-      console.log('accessToken:', accessToken);
-      console.log('refreshToken:', refreshToken);
-      console.log('profile:', profile);
+      // Search query returns a promise
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record
+          // creates a new instance of a user, makes a record in mongoDB
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
