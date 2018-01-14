081dcf37ce32334f44b207f238230b6606043da3
ykeanu <ykhernan@ucla.edu>
2018-01-13 23:55:35 -0800

    Set up production and development environments

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,13 @@ module.exports = app => {
   // Callback to google to request user info
   app.get("/auth/google/callback", passport.authenticate("google"));
 
+  app.get("/api/logout", (req, res) => {
+    // Function attached automatically to request object by passport
+    req.logout();
+    // Test to see if you are no longer signed in, should return 'undefined'
+    res.send(req.user);
+  });
+
   // Test if req.user exists, if so cookie works
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
