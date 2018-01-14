c87eebe0727684bee882ded9b36c9948e2d67095
ykeanu <ykhernan@ucla.edu>
2018-01-13 20:43:58 -0800

    Configured cookie and handle auth

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+// Give us access to cookies
+const cookieSession = require("cookie-session");
+// Tell passport to make use of them
+const passport = require("passport");
 const keys = require("./config/keys.js");
-// Order matters to load in user model before executing passport
+// Order matters, need to load in user model before executing passport
 require("./models/User");
 require("./services/passport");
 
@@ -9,6 +13,20 @@ mongoose.connect(keys.mongoURI);
 
 // Sets up configuration that will listen for incoming requests
 const app = express();
+
+// Allows use of cookies
+app.use(
+  // Configuration object for cookie
+  cookieSession({
+    // How long cookie exists in browser before automatically expired (units: milliseconds)
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    // 30 days conversion to milliseconds
+    keys: [keys.cookieKey]
+  })
+);
+// Instructs passport to make use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Require returns a function, and we call it with the (app) object
 require("./routes/authRoutes")(app);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,19 @@
-const passport = require('passport');
+const passport = require("passport");
 
-module.exports = (app) => {
-// User hits this route to begin oauth process
+module.exports = app => {
+  // User hits this route to begin oauth process
   app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
     })
   );
 
   // Callback to google to request user info
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get("/auth/google/callback", passport.authenticate("google"));
 
+  // Test if req.user exists, if so cookie works
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
