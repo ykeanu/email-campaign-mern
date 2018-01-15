4147501939fd867b8b683ee00fbc71e8c947d7e8
ykeanu <ykhernan@ucla.edu>
2018-01-14 20:56:54 -0800

    Created redux store + wire up authReducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Connect redux store, render provider tag
+// Provider is the "glue"
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
+
+// Create store arguments: 
+// (1) all different reducers in app 
+// (2) initial state of app, good for server side rendering 
+// (3) Middleware
+const store = createStore(reducers, {}, applyMiddleware());
 
 // Two arguments: (1) root component, (2) where we attempt to render inside dom
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  // React component that reads changes frmo redux store, and re-renders to all child components
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
+);
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,7 @@
+// Arguemnts: (1) state object with initial state (2) action object
+export default function (state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
