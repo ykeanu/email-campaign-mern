fb1cdeee791780a557b7ac04d48e05407e4eb25f
ykeanu <ykhernan@ucla.edu>
2018-01-14 22:58:53 -0800

    Created Header component + imported Materialize CSS library

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
 // Front end, we are making use of the front-end because of webpack + babel (ex: import)
 // Back-end, only has support with common js (ex: require)
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
+    // Div used for CSS
     <div>
-      Hi There!
+      <BrowserRouter>
+        {/* Routing rules */}
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path ="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 // Connect redux store, render provider tag
