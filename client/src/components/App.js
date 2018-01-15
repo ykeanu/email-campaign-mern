79ca7d8f0c5d8ed919e140837485a4f676c6bf8f
ykeanu <ykhernan@ucla.edu>
2018-01-14 23:26:30 -0800

    Refactor App to class-based component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 // Front end, we are making use of the front-end because of webpack + babel (ex: import)
 // Back-end, only has support with common js (ex: require)
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -8,21 +8,29 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-  return (
-    // Div used for CSS
-    <div>
-      <BrowserRouter>
-        {/* Routing rules */}
-        <div className="container">
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path ="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  
+  // Instant component mounts on the screen grab current user
+  componentDidMount() {
+
+  }
+  
+  render() {
+    return (
+      // Div used for CSS
+      <div>
+        <BrowserRouter>
+          {/* Routing rules */}
+          <div className="container">
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path ="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 };
 
 export default App;
